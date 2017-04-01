c4fe2494a52d4156650ce54c17d30db28ef50593
yeduvaka <yeduvaka@princeton.edu>
2017-04-01 01:50:43 -0400

    Edge Detection added

--- papr.py
+++ papr.py
@@ -1,13 +1,22 @@
-import cv2
-
 import numpy as np
 import cv2
 import math
 
-while(True):
-    ret,frame = cap.read()
+cap = cv2.VideoCapture(1)
 
-    cv2.imshow('Frame',frame)
+while(True):
+    ret,img = cap.read()
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    gray = np.float32(gray)
+    dst = cv2.cornerHarris(gray,2,3,0.04)
+
+    # Threshold for an optimal value, it may vary depending on the image.
+    img[dst>0.01*dst.max()]=[0,0,255]
+
+
+    cv2.imshow('Frame',img)
     if cv2.waitKey(1) &0xFF == ord('q'):
         break
 
