d822f968231c8308bc359a12744551a581efe156
Nicholas Jiang <nj3@princeton.edu>
2017-04-01 12:52:00 -0400

    added mouse movement and clicking API and corner detection

--- papr.py
+++ papr.py
@@ -1,20 +1,28 @@
-import numpy as np
 import cv2
-import math
 
 cap = cv2.VideoCapture(1)
+contour = []
+k_thresh = 150 # adjust for lighting
 
 while(True):
     ret,img = cap.read()
+    img = cv2.flip(img,1)
 
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.threshold(blurred, k_thresh, 255, cv2.THRESH_BINARY)[1]
+    edges = cv2.Canny(thresh, 50, 200)
+    (contours, _) = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    gray = np.float32(gray)
-    dst = cv2.cornerHarris(gray,2,3,0.04)
+    if len(contours) > 0 and len(contours) < 5:
+        contour = max(contours, key=len)
 
-    # Threshold for an optimal value, it may vary depending on the image.
-    img[dst>0.01*dst.max()]=[0,0,255]
-
+    if len(contour):
+        peri = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.04 * peri, True)
+        if len(approx) == 4:
+            for point in approx:
+                cv2.circle(img, (point[0][0], point[0][1]), 5, (0,0,255), 3)
 
     cv2.imshow('Frame',img)
     if cv2.waitKey(1) &0xFF == ord('q'):
